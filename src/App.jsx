66bfb040cec8f974172708a9fe1d66926398fa70
ggomez0 <gaspargomez2000@outlook.com>
2023-08-11 17:40:43 -0300

    latlong

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
-import './App.css'
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [latlon, setlatlong] = useState(null);
 
-  return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
+  useEffect(() => {
+      // Obtenemos la ubicación actual del usuario
+      navigator.geolocation.getCurrentPosition(
+        async (position) => {
+          setlatlong(position.coords);
+        })}, []);
 
-  </h1>
-  )
-}
+    return(
+    <div className='justify-center text-center flex flex-col h-screen dark:text-white dark:bg-slate-900 white:bg-slate-100 white:text-black'>
+      <h1 className='text-3xl'>Latitud: {latlon?.latitude}</h1>
+      <h1 className='text-3xl'>Longitud: {latlon?.longitude}</h1>
+      <h1>Exactitud de medición: {(Math.floor(latlon?.accuracy))/100}%</h1>
+      <button className='btn'>Actualizar ubicación</button>
 
 
-export default App
+    </div>
+    )
+
+
+  }
+export default App;
